692f4ce2f1ebf81cbed301dc2db60c9263d39df5
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2020-09-28 13:25:38 +0200

    dodany widok strony głównej. Edytowane pliki: views.py i urls.py

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -9,3 +9,6 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+def main_page(request):
+    return render(request, "index.html")
