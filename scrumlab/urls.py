531598b0163e7c75728f2e0f3929454a452bccbf
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2020-10-03 11:51:43 +0200

    Dodany widok obsługujący usuwanie posiłku z konkretnego planu

--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from jedzonko.views import (
     IndexView, main_page, Dashobard, recipe_list, plan_list, recipe_add, RecipeDetails,
-    recipe_modify, plan_details, plan_add, AddMealToPlan, page,
+    recipe_modify, plan_details, plan_add, AddMealToPlan, page, delete_recipeplan
 )
 
 
@@ -35,5 +35,6 @@ urlpatterns = [
     path('plan/add/', plan_add),
     path('plan/add-recipe/', AddMealToPlan.as_view()),
     path('<slug:slug>/', page),
+    path('recipeplan/<int:recipeplan_id>/delete/', delete_recipeplan)
 
 ]
